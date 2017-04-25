1565771f54eae0b354480842531c24dc686d3c14
Marius Adam <adamhoreamarius@gmail.com>
2017-04-26 01:46:41 +0300

    Initial commit

---
+++ src/main/java/org/ubb/courses/ai/Main.java
@@ -0,0 +1,10 @@
+package org.ubb.courses.ai;
+
+/**
+ * @author Marius Adam
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ssssss");
+    }
+}
---
+++ src/test/java/org/ubb/courses/ai/MainTest.java
@@ -0,0 +1,7 @@
+package org.ubb.courses.ai;
+
+/**
+ * @author Marius Adam
+ */
+public class MainTest {
+}
